34d0ded31301922b343575134ff8275c0ca94946
nnthach173 <83439525+nnthach173@users.noreply.github.com>
2021-05-06 23:55:16 +0700

    commit

---
+++ JavaSwing11_textfields/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//JTextField = a GUI textbox component that can be used to add, set, or get text
+	
+		MyFrame frame = new MyFrame();
+	
+	}
+
+}
